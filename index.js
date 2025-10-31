682fd2395102df510cc330c5d6eb68812d0a80af
Abdul Moiz <moiz05366@gmail.com>
2025-10-31 16:42:09 +0500

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import 'dotenv/config';
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://expense-tracker-b-3.netlify.app/"],
+    origin: ["http://localhost:5173", "https://expense-tracker-b-3.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
@@ -19,8 +19,12 @@ app.use("/api", routes);
 // connect database
 connectDB();
 
-app.get("/", (req , res) => {
+app.get("/", (req, res) => {
     res.send(new Date().toString());
+})
+
+app.listen(5000, () => {
+    console.log("Server is runing");
 })
 
 export default app;
--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     imageUrl: { type: String },
-    password: { type: String, required: true }
+    password: { type: String, required: true, minlength: 6 }
 }, { timestamps: true });
 
 export default mongoose.model("User", UserSchema);
